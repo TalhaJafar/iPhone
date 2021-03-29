dbb9198cf43aaba9c907cd6b61e9a91094988f9c
Talha Jafar <talhajafar37@outlook.com>
2021-03-29 21:22:48 +0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './globalStyles.scss'
+import './App.scss';
+
+function App() {
+  return (
+    <>
+      <div className="app">
+        abc
+      </div>
+      <div className="extending_func">
+        abc
+      </div>
+    </>
+  );
+}
+
+export default App;
